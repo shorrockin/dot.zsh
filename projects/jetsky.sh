1a6e0957d40b9cf5816bce4b11b800735d7c444a
Chris Shorrock <chris.shorrock@gmail.com>
2012-01-03 08:46:05 -0800

    fixed upgrade

--- projects/jetsky.sh
+++ projects/jetsky.sh
@@ -1,13 +1,8 @@
-if [ -z $JETSKY_HOME ]
-then
-    JETSKY_HOME="~/Work/jetsky"
-fi
-
-alias jetsky='cd $JETSKY_HOME'
+alias jetsky='cd ~/Work/jetsky'
 alias jetsky-server='jetsky && ./cake servers'
 alias jetsky-watch='jetsky && ./cake watch'
 alias jetsky-console='jetsky && ./node_modules/.bin/coffee'
-alias jetsky-upgrade='ssh jetsky@shorrockin.com "./deploy.sh"'
+alias jetsky-upgrade='ssh jetsky "./deploy.sh"'
 alias jetsky-mongo-log='/usr/local/Cellar/mongodb/2.0.1-x86_64/bin/mongod run --config /usr/local/Cellar/mongodb/2.0.1-x86_64/mongod.conf -v --rest'
 
 make-coffee-tags() {
