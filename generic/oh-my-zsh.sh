f34a3016fee46b7c434aae5b02d24a356206e9e9
Chris Shorrock <chris.shorrock@gmail.com>
2012-01-17 13:35:31 -0800

    changed oh-my-zsh theme to a one-line with information right-aligned

--- generic/oh-my-zsh.sh
+++ generic/oh-my-zsh.sh
@@ -2,6 +2,6 @@
 # Set up the prompt and plugins via oh-my-zsh
 # -----------------------------------------------
 ZSH=$HOME/.oh-my-zsh
-ZSH_THEME="candy"
+ZSH_THEME="terminalparty"
 plugins=(git osx ruby rails3 npm rvm github brew node)
 source $ZSH/oh-my-zsh.sh
