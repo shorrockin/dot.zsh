73ae4d9260b26ffd1c94b6bada6294d8bbe6199a
Chris Shorrock <chris.shorrock@gmail.com>
2013-05-07 08:24:53 -0700

    cleaned up some project files

--- projects/chimp.sh
+++ projects/chimp.sh
@@ -4,39 +4,39 @@ ch() {
   cd ~/Work/chimp
   tabname "chimp ($1)"
 
-  if [[ $1 == 'server' ]]
-  then
+  if [[ $1 == 'server' ]]; then
     ./script/rails server
-  elif [[ $1 == 'console' ]]
-  then
+  elif [[ $1 == 'console' ]]; then
     ./script/rails console
-  elif [[ $1 == 'test' ]]
-  then
+  elif [[ $1 == 'test' ]]; then
     shift
     rspec $@
-  elif [[ $1 == 'template' ]]
-  then
+  elif [[ $1 == 'template' ]]; then
     echo "todo subject line here ($2)" > ~/Work/chimp/app/views/email_templates/$2.subject.liquid
     echo "todo text content here ($2)" > ~/Work/chimp/app/views/email_templates/$2.text.liquid
     echo "todo html content here ($2)" > ~/Work/chimp/app/views/email_templates/$2.html.liquid
-  elif [[ $1 == 'init' ]]
-  then
+  elif [[ $1 == 'init' ]]; then
     mysql.server start
     rake ts:start
-  elif [[ $1 == 'sync-avatars' ]]
-  then
+  elif [[ $1 == 'sync-avatars' ]]; then
     rsync -ave ssh deploy@prod:/home/deploy/chimp/shared/system/ /Users/chrisshorrock/Work/chimp/public/system/
-  elif [[ $1 == 'sync-avatars-disco' ]]
-  then
+  elif [[ $1 == 'sync-avatars-disco' ]]; then
     rsync -ave ssh /Users/chrisshorrock/Work/chimp/public/system/ deploy@disco:/home/deploy/chimp/shared/system/
-  elif [[ $1 == 'make-db' ]]
-  then
+  elif [[ $1 == 'make-db' ]]; then
     mysql -uroot -proot -e "create database $2 character set utf8 collate utf8_general_ci"
-  elif [[ $1 == 'browser-stack' ]]
-  then
+  elif [[ $1 == 'browser-stack' ]]; then
     java -jar ~/Work/BrowserStackTunnel.jar $BROWSER_STACK_KEY chimp.loc,3000,0
-  elif [[ $# != 0 ]]
-  then
-    rake $@
+  elif [[ $1 == 'routes' ]]; then
+    if [[ $# == 2 ]]; then
+      rake routes | grep $2
+    else
+      rake routes
+    fi
+  elif [[ $1 == 'spork' ]]; then
+    spork
+  elif [[ $1 == 'mailcatcher' ]]; then
+    mailcatcher
+  elif [[ $# != 0 ]]; then
+    rails $@
   fi
 }
--- projects/jetsky.sh
+++ projects/jetsky.sh
@@ -2,32 +2,24 @@ ab() {
   cd ~/Work/atomic-brawl
   tabname "brawl ($1)"
 
-  if [[ $1 == 'mongo' ]]
-  then
+  if [[ $1 == 'mongo' ]]; then
     /usr/local/Cellar/mongodb/2.0.3-x86_64/bin/mongod
-  elif [[ $1 == 'mongo-log' ]]
-  then
+  elif [[ $1 == 'mongo-log' ]]; then
     /usr/local/Cellar/mongodb/2.0.3-x86_64/bin/mongod run --config /usr/local/Cellar/mongodb/2.0.3-x86_64/mongod.conf -v --rest    
-  elif [[ $1 == 'coffee' ]]
-  then
+  elif [[ $1 == 'coffee' ]]; then
     ./node_modules/.bin/coffee
-  elif [[ $1 == 'browser-stack' ]]
-  then
+  elif [[ $1 == 'repl' ]]; then
+    ./repl
+  elif [[ $1 == 'browser-stack' ]]; then
     java -jar ~/Work/BrowserStackTunnel.jar $BROWSER_STACK_KEY atomicbrawl.loc,40000,0,game.atomicbrawl.loc,50000,0,asset.atomicbrawl.loc,10000,0
-  elif [[ $1 == 'generate-art-descriptions' ]]
-  then
+  elif [[ $1 == 'generate-art-descriptions' ]]; then
     pie
     pie --dir /Users/chrisshorrock/Dropbox/atomic_brawl/patswerk/cards art_descriptions
-  elif [[ $1 == 'rename-card' ]]
-  then
+  elif [[ $1 == 'rename-card' ]]; then
     mv src/cards/$1.coffee src/cards/$2.coffee
     mv assets/images/cards/$1.png assets/images/cards/$2.png
     mv src/test/cards/$1.coffee src/test/cards/$2.coffee
-  elif [[ $1 == 'pie' ]]
-  then
-    pie
-  elif [[ $# != 0 ]]
-  then
+  elif [[ $# != 0 ]]; then
     pie $@
   fi 
 }
