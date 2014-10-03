8d44bc073edcf3a6a510b210cc8a51f4387d56b7
Chris Shorrock <chris.shorrock@gmail.com>
2014-10-03 09:47:55 -0700

    updated project files to control vagrant in more consistent fashion

--- projects/chimp.sh
+++ projects/chimp.sh
@@ -2,9 +2,17 @@ export VM_MEMORY=3072
 
 ch() {
   cd ~/Work/chimp.net
+  tabname "chimp (osx)"
 
   if [[ $# == 0 ]]; then
+    # nothing
+  elif [[ $1 == 'start' ]]; then
+    vagrant up
+    tabname "chimp (vagrant)"
+    /usr/bin/ssh vagrant
     tabname "chimp (osx)"
+  elif [[ $1 == 'stop' ]]; then
+    vagrant halt
   else
     tabname "chimp ($1)"
     /usr/bin/ssh $@
--- projects/lmc.sh
+++ projects/lmc.sh
@@ -1,8 +1,16 @@
 lmc() {
   cd ~/Work/lmc
+  tabname "lmc (osx)"
 
   if [[ $# == 0 ]]; then
+    # nothing
+  elif [[ $1 == 'start' ]]; then
+    vagrant up
+    tabname "lmc (vagrant)"
+    /usr/bin/ssh vagrant
     tabname "lmc (osx)"
+  elif [[ $1 == 'stop' ]]; then
+    vagrant halt
   else
     tabname "lmc ($1)"
     /usr/bin/ssh $@
