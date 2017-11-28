194ee9b782278a271f0ea44d3d02d7e1a5a156a3
Chris Shorrock <cs@stripe.com>
2017-11-27 22:16:57 -0800

    removed syntax highlighting

--- generic/oh-my-zsh.sh
+++ generic/oh-my-zsh.sh
@@ -7,7 +7,7 @@ ZSH_THEME="agnoster"
 DISABLE_AUTO_TITLE="true"
 
 source $ZSH/oh-my-zsh.sh
-source $ZSH/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+# source $ZSH/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
