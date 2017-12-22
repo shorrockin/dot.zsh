cf12340e24db5590fb5b2111dae7fde152ab38f3
Chris Shorrock <cs@stripe.com>
2017-12-22 08:33:01 -0800

    removed emacsclient alias

--- osx.sh
+++ osx.sh
@@ -1,8 +1,7 @@
-alias emacs='emacsclient --alternate-editor=emacs'
 alias colored='ack --flush --passthru --color --color-match=blue "DEBUG" | ack --flush --passthru --color --color-match=yellow "WARN(ING)?" |  ack --flush --passthru --color --color-match=red "ERROR" | ack --flush --passthru --color --color-match=red "^.*FAILURE.*" | ack --flush --passthru --color --color-match=green "BUILD SUCCESSFUL" | ack --flush --passthru --color --color-match=green "^Tests run: .*" | ack --flush --passthru --color --color-match=blue "\-\-\-*"'
 
-tail() { 
-  /usr/bin/tail $@ | colored 
+tail() {
+  /usr/bin/tail $@ | colored
 }
 
 tabname() {
